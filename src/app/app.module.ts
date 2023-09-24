cdf07abaab57e24b04b68593ba50a626fd9990e6
Rushikesh-kesbhat-4189-pcccs-Tybsc <rushikeshkesbhat7775@gmail.com>
2023-09-24 16:06:34 +0530

    angular initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageModule } from 'src/pages/page/page.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/material/material/material.module';
+import { SidebarComponent } from 'src/pages/layout/sidebar/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PageModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
